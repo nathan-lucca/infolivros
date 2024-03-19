3b6cb4f2dcb30fa323a518ebf510b077dea04a4f
Nathan Lucca Covre <nathancovre2018@gmail.com>
2024-03-19 01:21:02 -0300

    Bloqueando acesso ao código fonte da página

--- assets/js/index.js
+++ assets/js/index.js
@@ -16,7 +16,7 @@ async function carregarLivros(startIndex, query) {
     const maxResults = 15;
     const url = `https://www.googleapis.com/books/v1/volumes?q=${query}&startIndex=${startIndex}&maxResults=${maxResults}&key=${apiKey}`;
 
-    await new Promise(resolve => setTimeout(resolve, 2000));
+    await new Promise((resolve) => setTimeout(resolve, 2000));
 
     const response = await fetch(url);
     if (!response.ok) {
@@ -196,6 +196,33 @@ document
       atualizarLista();
     }
   });
+
+// Evento disparado ao pressionar as teclas de atalho para abrir o Inspecionar
+window.addEventListener(
+  "keydown",
+  function (event) {
+    if (event.keyCode === 123) {
+      // Código da tecla F12
+      alert("Acesso ao código fonte não permitido!");
+      event.preventDefault();
+    } else if (event.ctrlKey && event.shiftKey && event.keyCode === 73) {
+      // Ctrl+Shift+I
+      alert("Acesso ao código fonte não permitido!");
+      event.preventDefault();
+    }
+  },
+  false
+);
+
+// Evento disparado ao clicar com o botão direito do mouse
+window.addEventListener(
+  "contextmenu",
+  function (event) {
+    event.preventDefault();
+    alert("Acesso ao código fonte não permitido!");
+  },
+  false
+);
 
 window.onload = function () {
   document
